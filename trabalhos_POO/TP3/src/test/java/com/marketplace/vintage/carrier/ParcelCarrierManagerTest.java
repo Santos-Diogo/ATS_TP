9ee2113500c0cc63ab3fc4ba1a57d85d3628c4f8
samba@hows <santos.diogo.21@outlook.com>
2024-05-23 18:57:17 +0100

    extensao dos testes sobre Carriers

--- trabalhos_POO/TP3/src/test/java/com/marketplace/vintage/carrier/ParcelCarrierManagerTest.java
+++ trabalhos_POO/TP3/src/test/java/com/marketplace/vintage/carrier/ParcelCarrierManagerTest.java
@@ -14,12 +14,16 @@ class ParcelCarrierManagerTest {
     void testParcelCarrierManager() {
         ParcelCarrierManager parcelCarrierManager = new ParcelCarrierManager();
 
-        String testName = "DHL";
+        String testNormal= "DHL";
+        String testPremium= "DHL-Deluxe";
 
-        parcelCarrierManager.registerParcelCarrier(ParcelCarrierFactory.createNormalParcelCarrier(testName));
+        parcelCarrierManager.registerParcelCarrier(ParcelCarrierFactory.createNormalParcelCarrier(testNormal));
+        parcelCarrierManager.registerParcelCarrier(ParcelCarrierFactory.createPremiumParcelCarrier(testPremium));
 
-        ParcelCarrier parcelCarrier = parcelCarrierManager.getCarrierByName(testName);
+        ParcelCarrier parcelCarrier = parcelCarrierManager.getCarrierByName(testNormal);
+        ParcelCarrier parcel_carrier_delux= parcelCarrierManager.getCarrierByName(testPremium);
         assertEquals("DHL", parcelCarrier.getName());
+        assertEquals("DHL-Deluxe", parcel_carrier_delux.getName());
 
         assertThrowsExactly(EntityNotFoundException.class, () -> parcelCarrierManager.getCarrierByName("UPS"));
         assertThrowsExactly(EntityAlreadyExistsException.class, () -> parcelCarrierManager.registerParcelCarrier(ParcelCarrierFactory.createNormalParcelCarrier(testName)));
